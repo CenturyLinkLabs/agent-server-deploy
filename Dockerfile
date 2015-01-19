291d79b59f8e155bbc18546cfdb22b264b2f2610
Chaitanya Akkineni <cakkineni@gmail.com>
2015-01-19 08:54:18 -0600

    Updating github url

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ RUN opkg-install bash
 RUN mkdir -p /etc/ssl && mkdir -p /etc/ssl/certs
 ADD certs /etc/ssl/certs/
 ENV SHELL /bin/bash
-ADD deployAgent deployAgent
+ADD deployAgent/deployAgent deployAgent
 RUN chmod +x deployAgent
 CMD "./deployAgent"
---
+++ deployAgent/deployAgent.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/CenturyLinkLabs/kube-cluster-deploy/deploy"
+	"github.com/CenturyLinkLabs/kube-cluster-deploy/utils"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	fmt.Printf("\nDeploying Agent Server")
+
+	puk, pk, _ := utils.CreateSSHKey()
+
+	fmt.Printf("\nSSH Key Created")
+
+	c := deploy.CenturyLink{
+		PrivateSSHKey: pk,
+		PublicSSHKey:  puk,
+		APIUsername:   os.Getenv("USERNAME"),
+		APIPassword:   os.Getenv("PASSWORD"),
+		GroupID:       os.Getenv("GROUP_ID"),
+		CPU:           1,
+		MemoryGB:      1,
+		TCPOpenPorts:  []int{3036, 8080},
+		ServerName:    "AGENT",
+	}
+
+	s, e := c.DeployVM()
+
+	if e != nil {
+		fmt.Printf("\n%s", e.Error())
+		panic(e)
+	}
+
+	utils.SetKey("AGENT_PRIVATE_KEY", pk)
+	utils.SetKey("AGENT_PUB_IP", s.PublicIP)
+
+	r := bufio.NewReader(os.Stdin)
+	text, _ := r.ReadString('\n')
+	fmt.Printf("STDIN:%s", text)
+	kv := strings.Split(text, "=")
+	utils.SetKey("AGENT_KUBE_API", kv[1])
+}

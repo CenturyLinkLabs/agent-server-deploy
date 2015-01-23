2e84a3d10f232cc3f81285e748282b96367cc938
Chaitanya Akkineni <cakkineni@gmail.com>
2015-01-22 16:19:46 -0800

    Updating binary

--- deployAgent/deployAgent.go
+++ deployAgent/deployAgent.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"encoding/base64"
 	"fmt"
 	"github.com/CenturyLinkLabs/kube-cluster-deploy/deploy"
 	"github.com/CenturyLinkLabs/kube-cluster-deploy/utils"
@@ -13,20 +14,21 @@ func main() {
 
 	fmt.Printf("\nDeploying Agent Server")
 
-	puk, pk, _ := utils.CreateSSHKey()
+	pk, puk, _ := utils.CreateSSHKey()
 
 	fmt.Printf("\nSSH Key Created")
 
 	c := deploy.CenturyLink{
-		PrivateSSHKey: pk,
-		PublicSSHKey:  puk,
-		APIUsername:   os.Getenv("USERNAME"),
-		APIPassword:   os.Getenv("PASSWORD"),
-		GroupID:       os.Getenv("GROUP_ID"),
-		CPU:           1,
-		MemoryGB:      1,
-		TCPOpenPorts:  []int{3036, 8080},
-		ServerName:    "AGENT",
+		PrivateSSHKey:  pk,
+		PublicSSHKey:   puk,
+		APIUsername:    os.Getenv("USERNAME"),
+		APIPassword:    os.Getenv("PASSWORD"),
+		GroupID:        os.Getenv("GROUP_ID"),
+		CPU:            1,
+		MemoryGB:       1,
+		TCPOpenPorts:   []int{3036, 8080},
+		ServerName:     "AGENT",
+		ServerTemplate: "UBUNTU-14-64-TEMPLATE",
 	}
 
 	s, e := c.DeployVM()
@@ -36,12 +38,20 @@ func main() {
 		panic(e)
 	}
 
-	utils.SetKey("AGENT_PRIVATE_KEY", pk)
-	utils.SetKey("AGENT_PUB_IP", s.PublicIP)
+	//utils.SetKey("AGENT_PRIVATE_KEY", base64.StdEncoding.EncodeToString([]byte(pk)))
+	utils.SetKey("AGENT_PUBLIC_KEY", base64.StdEncoding.EncodeToString([]byte(puk)))
+	utils.SetKey("AGENT_PUBLIC_IP", s.PublicIP)
 
-	r := bufio.NewReader(os.Stdin)
-	text, _ := r.ReadString('\n')
-	fmt.Printf("STDIN:%s", text)
-	kv := strings.Split(text, "=")
-	utils.SetKey("AGENT_KUBE_API", kv[1])
+	line := ""
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		line, e = reader.ReadString('\n')
+		if e != nil || strings.ContainsAny(line, "AGENT_KUBER_API") {
+			break
+		}
+	}
+	fmt.Printf("STDIN:%s", line)
+	kv := strings.Split(line, "=")
+	utils.SetKey("AGENT_KUBER_API", kv[1])
 }
